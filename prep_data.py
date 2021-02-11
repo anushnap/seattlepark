238959e1eebd137d8cbd42b3cf0f9cc9fa39151f
anushnap <anushnap@uw.edu>
2021-02-11 15:55:06 -0800

    created prep_data.py to clean data set

---
+++ prep_data.py
@@ -0,0 +1,19 @@
+# Set up data cleaning process
+import os
+import pandas as pd
+
+
+# Split Study_Area on - character and save as separate columns
+def clean_area_col(parking):
+    areas = parking['Study_Area'].str.split('-', expand = True)
+    parking['Area'] = areas[0].str.strip()
+    parking['Sub-Area'] = areas[1].str.strip() + ' - ' + areas[2].str.strip()
+
+
+if __name__ == "__main__":
+    wd = os.getcwd()
+
+    # Fix dtypeWarning error (22, 23) have mixed types
+    parking = pd.read_csv(wd + "/data/Annual_Parking_Study_Data.csv")
+    clean_area_col(parking)
+    print(parking.columns)
