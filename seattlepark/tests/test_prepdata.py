09524099c6380e32fc56e931818787cfe7733aa9
anushnap <anushnap@uw.edu>
2021-02-11 17:34:26 -0800

    changed file structure

---
+++ seattlepark/tests/test_prepdata.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+import prep_data as prd
+
+class TestFilePrep(unittest.TestCase):
+
+    def test_raises_Missing_column(self):
+        filepath = os.getcwd()
+
+        data = pd.read_csv(filepath + "/data/Annual_Parking_Study_Data.csv")
+        missing_col = data.drop('Study_Area', axis = 1)
+
+        self.assertRaises(prd.MissingStudyArea, prd.clean_area_col, missing_col)
+
+
+if __name__ == "__main__":
+    unittest.main()
