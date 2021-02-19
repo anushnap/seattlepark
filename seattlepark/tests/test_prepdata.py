dad23be51e43279ce13622034f41fa5e51057858
anushnap <anushnap@uw.edu>
2021-02-18 16:53:01 -0800

    fixed unittest structure and .travis file

--- seattlepark/tests/test_prepdata.py
+++ seattlepark/tests/test_prepdata.py
@@ -1,14 +1,14 @@
 import unittest
 import os
 import pandas as pd
-import prep_data as prd
+import seattlepark.prep_data as prd
 
 class TestFilePrep(unittest.TestCase):
 
     def test_raises_Missing_Study_Area(self):
-        filepath = os.getcwd()
+        filepath = os.path.join(os.path.dirname(__file__), "data/Annual_Parking_Study_Data.csv")
 
-        data = pd.read_csv(filepath + "/data/Annual_Parking_Study_Data.csv")
+        data = pd.read_csv(filepath)
         missing_col = data.drop('Study_Area', axis = 1)
 
         self.assertRaises(prd.MissingStudyArea, prd.clean_area_col, missing_col)
