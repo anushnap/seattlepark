37a33f8716711927184e35abd3f560b9eb4b27b4
anushnap <anushnap@uw.edu>
2021-02-26 14:03:04 -0800

    updated python versions

---
+++ seattlepark/tests/test_slice_df.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+import pandas as pd
+import seattlepark.slice_df as slc
+
+class TestDataSlice(unittest.TestCase):
+
+    def test_raises_Invalid_Area_Exception(self):
+        pass
+
+    def test_raises_Invalid_DayType_Exception(self):
+        pass
+
+    def test_raises_No_Search_Results_Exception(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
