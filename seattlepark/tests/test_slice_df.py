ced43411ca5340053d99ecd274e0c04a7ecefa1e
anushnap <anushnap@uw.edu>
2021-02-26 18:25:17 -0800

    added some unittests to test_slice_df

--- seattlepark/tests/test_slice_df.py
+++ seattlepark/tests/test_slice_df.py
@@ -6,10 +6,10 @@ import seattlepark.slice_df as slc
 class TestDataSlice(unittest.TestCase):
 
     def test_raises_Invalid_Area_Exception(self):
-        pass
+        self.assertRaises(slc.InvalidAreaError, slc.slice_df, 'Invalid neighborhood', 'any', 0)
 
     def test_raises_Invalid_DayType_Exception(self):
-        pass
+        self.assertRaises(slc.InvalidDayTypeError, slc.slice_df, 'Ballard', 'any', -1)
 
     def test_raises_No_Search_Results_Exception(self):
         pass
