e1b9de0ceb459637ece2d2f6dbf6ee906472589f
anushnap <anushnap@uw.edu>
2021-03-04 21:10:54 -0800

    created empty tests, linted existing tests

---
+++ seattlepark/tests/test_parking_spot.py
@@ -0,0 +1,29 @@
+"""
+I'm not actually sure if we need to do a unit test for a class where all it
+has is an init that assigns the parameters. Just writing in a bunch of 
+useless tests anyway
+"""
+import unittest
+from seattlepark.src.parking_spot import ParkingSpot
+
+
+class TestParkingSpotInit(unittest.TestCase):
+    """ Test the ParkingSpot constructor"""
+    def test_parking_spot(self):
+        test_ps = ParkingSpot(
+            1, [[1, 3], [2, 4]], 
+            "1ST AVE BETWEEN SENECA ST AND UNIVERSITY ST",
+            1, 1)
+        
+        self.assertEqual(test_ps.calculated_distance, 1)
+        self.assertEqual(test_ps.street_meet_expect_coordinates,
+                         [[1, 3], [2, 4]])
+        self.assertEqual(test_ps.street_name, 
+                         "1ST AVE BETWEEN SENECA ST AND UNIVERSITY ST")
+        self.assertEqual(test_ps.street_lat_mid, 1)
+        self.assertEqual(test_ps.street_lon_mid, 1)
+        self.assertEqual(test_ps.spaceavail, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
