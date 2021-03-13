ad4965eccdba36795677018a958ea4591dac0914
anushnap <anushnap@uw.edu>
2021-03-12 17:34:49 -0800

    added coordinates_util tests

--- seattlepark/tests/test_coordinates_util.py
+++ seattlepark/tests/test_coordinates_util.py
@@ -5,24 +5,75 @@ import pandas as pd
 import numpy as np
 import haversine as hs
 
+class TestCoordinate:
+
+    def __init__(self, lat, long):
+        self.latitude = lat
+        self.longitude = long
+
+
 class CoordinatesUtilTest(unittest.TestCase):
 
+    def setUp(self):
+        self.cu = CoordinatesUtil()
+    
     def test_cal_distance(self):
+        """Calcs haversine distance on tuple"""
         test_loc1 = (45.8, 4.8)
         test_loc2 = (48.9, 2.4)
 
-        # Can I actually use mock like this in calls that require "self"?
         self.assertEqual(hs.haversine(test_loc1, test_loc2, unit = "mi"),
-                         CoordinatesUtil.cal_distance(Mock(),
-                                                      test_loc1,
-                                                      test_loc2))
+                         self.cu.cal_distance(test_loc1, test_loc2))
     
+    def test_cal_distance_takes_list(self):
+        """Calcs haversine distance on list"""
+        test_loc1 = [45.8, 4.8]
+        test_loc2 = [48.9, 2.4]
+
+        self.assertEqual(hs.haversine(test_loc1, test_loc2, unit = "mi"),
+                         self.cu.cal_distance(test_loc1, test_loc2))
+
+    def test_cal_distance_raises_value_error(self):
+        """Raise ValueError when passed iterables > 2"""
+
+        self.assertRaises(ValueError, 
+                          self.cu.cal_distance, (1, 2, 3), (1, 2, 3))
+        self.assertRaises(ValueError, 
+                          self.cu.cal_distance, [1, 2, 3], [1, 2, 3])
+    
+    def test_cal_distance_raises_type_error(self):
+        """Raise TypeError when passed strings"""
+        self.assertRaises(TypeError, 
+                          self.cu.cal_distance, ('1', '2'), (1, 2))
+        self.assertRaises(TypeError,
+                          self.cu.cal_distance, (1, 2), ('1', '2'))
+
     # GET QIAOHUIS HELP
     def test_get_destination_coordinates(self):
         pass
 
     def test_get_parking_spots(self):
         pass
+
+    def test_sea_parking_geocode(self):
+        coordinates_mapping = self.cu.sea_parking_geocode()
+        self.assertTrue(len(coordinates_mapping) > 0)
+        for key, value in coordinates_mapping.items():
+            self.assertTrue(key is not None)
+            self.assertTrue(value is not None)
+            self.assertEqual(len(value), 2)
+            self.assertEqual(len(value[0]), 2)
+            self.assertEqual(len(value[1]), 2)
+
+    def test_get_destination_coordinates(self):
+        cu = CoordinatesUtil()
+        # Mock member variable geo_locator of CoordinatesUtil
+        cu.geo_locator = Mock()
+        # Mock geocode on the mocked member variable geo_locator to return value as Coordinate(Point(1.1, 1.2)).
+        cu.geo_locator.geocode.return_value = TestCoordinate(1.1, 1.2)
+
+        coordinates = cu.get_destination_coordinates("Some Address")
+        self.assertEqual(coordinates, [1.1, 1.2])
 
 
 if __name__ == "__main__":
--- seattlepark/tests/test_coordinates_util_new.py
+++ seattlepark/tests/test_coordinates_util_new.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock, patch
-from coordinates_util import *
+from coordinates_util import CoordinatesUtil
 
 
 class Coordinate:
