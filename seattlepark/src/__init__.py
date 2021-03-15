28dfe0af65c731e328c434575bf56674f436290b
anushnap <anushnap@uw.edu>
2021-03-14 17:50:26 -0700

    removed histogram.py import

--- seattlepark/src/__init__.py
+++ seattlepark/src/__init__.py
@@ -1,4 +1,3 @@
 from seattlepark.src import coordinates_util
 from seattlepark.src import parking_recommender
 from seattlepark.src import sea_street_parking_line
-from seattlepark.src import histogram
